8ed65f9f66eea1922a2167b3f4a6bdbf52746685
KOFFI HAMENOU <164899950+Ehonam@users.noreply.github.com>
2024-06-07 11:59:16 +0200

    Add files via upload

--- Koffi-SQL/Requête #10.sql
+++ Koffi-SQL/Requête #10.sql
@@ -1,3 +1,3 @@
--- Ajouter un ingrédient en base de données : Poivre, unité : cuillère à café, prix : 2.5 €
+-- Ajouter un ingrédient en base de données : Poivre noir, unité : cuillère à café, prix : 2.5 €
 INSERT INTO ingredients (ingredient_name, unity, price)
 VALUES ('Poivre noir', '1 cuillère à café', 2.5);
