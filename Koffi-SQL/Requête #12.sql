d56a47be7a51ccad83e975a064b052fb53739019
KOFFI HAMENOU <164899950+Ehonam@users.noreply.github.com>
2024-06-07 14:51:36 +0200

    Add files via upload

---
+++ Koffi-SQL/Requête #12.sql
@@ -0,0 +1,13 @@
+-- Afficher le nombre de recettes par catégories : X entrées, Y plats, Z desserts
+/*
+SELECT id_category, COUNT(id_recipe)
+FROM recipe
+INNER JOIN category ON category.id_category = recipe.id_category
+GROUP BY category.id_category
+*/
+
+SELECT category_name, COUNT(id_recipe) AS nbRecettes
+FROM category
+INNER JOIN recipe ON category.id_category = recipe.id_category
+GROUP BY category.id_category /*on utilise GROUP BY à chaque fois qu'on utilise une fonction d'aggrégation comme COUNT, SUM, AVG...*/
+ORDER BY nbRecettes DESC
