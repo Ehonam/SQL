78043d647e4000cbb15e595c4a8b9cd0a8a2dcc7
KOFFI HAMENOU <164899950+Ehonam@users.noreply.github.com>
2024-06-07 09:29:42 +0200

    Update Requête #4.sql

--- Koffi-SQL/Requête #4.sql
+++ Koffi-SQL/Requête #4.sql
@@ -1,4 +1,6 @@
 
 /*Afficher les recettes dont le nom contient le mot « Salade » (peu importe où est situé le mot en question)*/
 
-SELECT
+SELECT * 
+FROM recipe 
+WHERE recipe_name LIKE '%Salade%';
